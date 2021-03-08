731ebb72237506ec9bf080bd4ce4809cbeaec583
Lucas Stellet <lustepe@gmail.com>
2021-03-08 12:05:44 -0300

    up

--- k8s/k8s-api/index.js
+++ k8s/k8s-api/index.js
@@ -2,25 +2,7 @@ const k8s = require('./kubernetes.service');
 const util = require('util');
 
 (async () => {
-  const deployments = await k8s.listDeployments('linkapi');
-  const jsonDeploy = JSON.stringify(
-    deployments.map(deployment => ({
-      name: deployment.metadata.name,
-      namespace: deployment.metadata.namespace,
-      spec: {
-        replicas: deployment.spec.replicas,
-        containers: deployment.spec.template.spec.containers.map(container => ({
-          name: container.name,
-          envFrom: container.envFrom,
-          image: container.image,
-          ports: container.ports,
-          resources: container.resources,
-        })),
-      },
-      status: {
-        conditions: deployment.status.conditions,
-      },
-    }))
-  );
-  console.log(jsonDeploy);
+  let deployments = await k8s.listDeployments('linkapi');
+  deployments = deployments.map(deploy => JSON.stringify(deploy));
+  console.log(deployments);
 })();
